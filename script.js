d4e9cd617daf1330a0161ec33801a6433fb18a02
hsiV <kelvishvargas23@gmail.com>
2025-09-29 12:42:07 +0800

    Modified the interactivity, styling, and layout

---
+++ script.js
@@ -0,0 +1,11 @@
+const shareButton = document.querySelector(".share-container");
+shareButton.classList.toggle("share-container-toggle");
+const shareIcon = document.querySelector(".share-icon-toggle");
+shareIcon.classList.toggle("share-icon-toggle");
+const popUp = document.querySelector(".hidden");
+
+shareButton.addEventListener("click", () =>{
+    shareButton.classList.toggle("share-container-toggle");
+    shareIcon.classList.toggle("share-icon-toggle");
+    popUp.classList.toggle("hidden");
+});
